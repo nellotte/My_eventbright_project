121459801ca31c2194593c5fa9e26de62862c284
nellotte <nelly.guerin@gmail.com>
2023-11-06 14:11:22 +0100

    first commit

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,6 +7,10 @@ class Event < ApplicationRecord
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 1000 }
   validates :location, presence: true
 
+  belongs_to :admin, class_name: "User", foreign_key: "user_id"
+  
+  has_many :attendance
+
   private
 
   def start_date_cannot_be_in_the_past
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
+  has_many :attendance
+  after_create :welcome_send
 
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
 end
